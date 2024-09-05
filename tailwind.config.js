fb051f2122867d2caf9e1a48774f00d860a4d760
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-09-04 20:55:08 -0500

    prueba de la página oficial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     extend: {
       screens: {
         'mobil-320': '320px',
-      }
+      },
+      backgroundColor: {
+        'azul-catolica': '#1B5CFF',
+        'blanco-catolica': '#F3F4F4',
+        'amarillo-catolica': '#F4DA40',
+        'verde-catolica': '#36A2BC',
+      },
+      colors: {
+        'azul-catolica': '#1B5CFF',
+        'blanco-catolica': '#F3F4F4',
+        'amarillo-catolica': '#F4DA40',
+        'verde-catolica': '#36A2BC',
+      },
+      height: {
+        '821': '821px',
+      },
     },
   },
   plugins: [],
